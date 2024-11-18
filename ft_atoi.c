e540f773f3b83ea221c104babe3f3c8a25a9f929
ChahirSaid <80861946+ChahirSaid@users.noreply.github.com>
2024-11-18 18:18:19 +0100

    handle ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -19,26 +19,37 @@ static int	ft_isspace(int c)
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	res;
+	int		i;
+	int		sign;
+	long	res;
+	int		digit_count;
 
 	i = 0;
 	sign = 1;
 	res = 0;
+	digit_count = 0;
+
 	while (ft_isspace(str[i]))
 		i++;
 	if (str[i] == '-')
 	{
-		sign = -sign;
+		sign = -1;
 		i++;
 	}
 	else if (str[i] == '+')
 		i++;
 	while (str[i] && ft_isdigit(str[i]))
 	{
-		res = res * 10 + (str[i] - 48);
+		res = res * 10 + (str[i] - '0');
+		digit_count++;
+		if (digit_count > 19)
+		{
+			if (sign == 1)
+				return (-1);
+			else
+				return (0);
+		}
 		i++;
 	}
-	return (res * sign);
+	return ((int)(res * sign));
 }
